e9374a8b85ca2577a88d8f3a257c6ba0bb638807
EliteTC <deadend1408@gmail.com>
2017-01-19 11:05:47 +0200

    functionality completed

---
+++ src/com/besedkin/MyServlet.java
@@ -0,0 +1,71 @@
+package com.besedkin;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class MyServlet extends HttpServlet {
+
+
+	private final static String LOGIN = "LOGIN";
+	private final static String PASSWORD = "PASSWORD";
+
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		String name = request.getParameter("uname");
+		String password = request.getParameter("psw");
+		response.setContentType("text/html;charset=UTF-8");
+
+		PrintWriter out = response.getWriter();
+
+		try {
+
+			out.println("<html>");
+
+			out.println("<head>");
+
+			out.println("<title>Greeting page</title>");
+
+			out.println("</head>");
+
+			out.println("<body>");
+
+			if(name.equals(LOGIN) && password.equals(PASSWORD)) {
+				out.println("<h1>Hello "+name+"!!!</h1>");
+			}
+			else {
+				out.println("User not recognized!!!");
+			}
+
+			out.println("</body>");
+
+			out.println("</html>");
+
+
+
+		} finally {
+
+			out.close();
+
+		}
+
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+		processRequest(request, response);
+	}
+
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+		processRequest(request, response);
+
+	}
+
+}
