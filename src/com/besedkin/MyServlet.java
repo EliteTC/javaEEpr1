e7f7fffc4e37adfe3f14fac43840ddc5fa852fc5
EliteTC <deadend1408@gmail.com>
2017-01-28 11:04:23 +0200

    added jsp

--- src/com/besedkin/MyServlet.java
+++ src/com/besedkin/MyServlet.java
@@ -2,6 +2,7 @@ package com.besedkin;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +16,7 @@ public class MyServlet extends HttpServlet {
 	private final static String LOGIN = "LOGIN";
 	private final static String PASSWORD = "PASSWORD";
 
-
+/*
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		String name = request.getParameter("uname");
 		String password = request.getParameter("psw");
@@ -55,16 +56,28 @@ public class MyServlet extends HttpServlet {
 		}
 
 	}
+	*/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-
-		processRequest(request, response);
+		String name = request.getParameter("uname");
+		String password = request.getParameter("psw");
+		response.setContentType("text/html;charset=UTF-8");
+		if(name.equals(LOGIN) && password.equals(PASSWORD)) {
+			request.setAttribute("message", "Hello "+name+"!!");
+		}
+		else {
+			request.setAttribute("message", "User not recognized!!");
+		}
+		
+		RequestDispatcher view = request.getRequestDispatcher("response.jsp");
+		view.forward(request, response);
+		//processRequest(request, response);
 	}
 
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-
-		processRequest(request, response);
+		doGet(request,response);
+		//processRequest(request, response);
 
 	}
 
